454263f533e5558fdf6cda1d584e476bb036f346
Kim Weible <kw00183@my.westga.edu>
2021-10-16 12:53:20 -0500

    CompositeGrade - stage new class, implement Grade interface and add in getValue method

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/CompositeGrade.java
@@ -0,0 +1,32 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * Class for the composite grade that implements Grade
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class CompositeGrade implements Grade {
+
+	private double grade;
+	
+	/**
+	 * Creates a new composite grade object
+	 * 
+	 * @param grade
+	 *            double value of grade
+	 */
+	public CompositeGrade(double grade) {
+		this.grade = grade;
+	}
+
+	/**
+	 * Method implemented from Grade to return a double grade value
+	 * 
+	 * @return grade
+	 *            double value of grade
+	 */
+	public double getValue() {
+		return this.grade;
+	}
+}
