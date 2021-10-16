d89fa1437a9ef88876e2435a872b29b350cc04d4
Kim Weible <kw00183@my.westga.edu>
2021-10-16 18:07:44 -0500

    TestSimpleGradeGetValue - stage JUnit test class for SimpleGrade tests for setting and getting grade value and testing constraints

---
+++ src/test/java/edu/westga/cs/schoolgrades/testing/TestSimpleGradeGetValue.java
@@ -0,0 +1,71 @@
+package edu.westga.cs.schoolgrades.testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs.schoolgrades.model.SimpleGrade;
+
+/**
+ * JUnit test class for Simple Grade model class getValue method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestSimpleGradeGetValue {
+
+	private SimpleGrade grade;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		this.grade = new SimpleGrade();
+	}
+
+	/**
+	 * Test to check that creating SimpleGrade results in 0.0 grade value
+	 */
+	@Test
+	public void createSimpleGradeShouldGetValue0() {
+		assertEquals(0.0, this.grade.getValue());
+	}
+
+	/**
+	 * Test to check that setting a valid grade returns set result
+	 */
+	@Test
+	public void setValidGradeshouldGetValueSet() {
+		this.grade.setValue(99.0);
+		assertEquals(99.0, this.grade.getValue());
+	}
+
+	/**
+	 * Test to check that setting an invalid negative grade returns 0.0 grade
+	 * value
+	 */
+	@Test
+	public void setInvalidNegativeGradeshouldGetValue0() {
+		try {
+			this.grade.setValue(-99.0);
+		} catch (IllegalArgumentException iae) {
+			assertEquals(
+					"grade must be positive or less than or equal to 100.0",
+					iae.getMessage());
+		}
+	}
+
+	/**
+	 * Test to check that setting an invalid high grade (grade over 100.0)
+	 * returns 0.0 grade value
+	 */
+	@Test
+	public void setInvalidHighGradeshouldGetValue0() {
+		try {
+			this.grade.setValue(105.0);
+		} catch (IllegalArgumentException iae) {
+			assertEquals(
+					"grade must be positive or less than or equal to 100.0",
+					iae.getMessage());
+		}
+	}
+}
