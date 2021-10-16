3868a7ef615b583b918236feafb152f3825808e8
Kim Weible <kw00183@my.westga.edu>
2021-10-16 13:28:10 -0500

    SumGradeStrategy - stage a new class to calculate the grades subtotal using sum

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/SumGradeStrategy.java
@@ -0,0 +1,18 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.ArrayList;
+
+/**
+ * Class for the strategy to sum all the grades
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class SumGradeStrategy implements GradeStrategy {
+
+	@Override
+	public double getSubtotal(ArrayList<Grade> grades) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
