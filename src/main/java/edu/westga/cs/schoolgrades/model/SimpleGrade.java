95e12cf8e646e456fc5e3166c3fb69cf5edf904d
Kim Weible <kw00183@my.westga.edu>
2021-10-16 12:42:10 -0500

    SimpleGrade - stage the class file, implement the Grade interface and extend the getValue method

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/SimpleGrade.java
@@ -0,0 +1,32 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * Class for the simple grade that implements Grade
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class SimpleGrade implements Grade {
+
+	private double grade;
+
+	/**
+	 * Creates a new simple grade that retrieves the grade value
+	 * 
+	 * @param grade
+	 *            double value of grade
+	 */
+	public SimpleGrade(double grade) {
+		this.grade = grade;
+	}
+
+	/**
+	 * Method implemented from Grade to return a double grade value
+	 * 
+	 * @return grade
+	 *            double value of grade
+	 */
+	public double getValue() {
+		return this.grade;
+	}
+}
