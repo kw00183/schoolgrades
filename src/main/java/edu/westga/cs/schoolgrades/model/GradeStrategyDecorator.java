8a52050e9479925908cb3f2560f7fd0fa61f8bff
Kim Weible <kw00183@my.westga.edu>
2021-10-16 14:25:50 -0500

    GradeStrategyDecorator - update the class to make it abstract, add in constructor that accepts the strategy, parameter for doAdditional method that accepts the strategy, and update the getSubtotal method to return the strategy subtotal

--- src/main/java/edu/westga/cs/schoolgrades/model/GradeStrategyDecorator.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/GradeStrategyDecorator.java
@@ -3,18 +3,38 @@ package edu.westga.cs.schoolgrades.model;
 import java.util.ArrayList;
 
 /**
- * Class for the strategy decorator to drop the lowest grade all the grades
+ * Class for the strategy decorator to add additional logic the grades
  * 
  * @author Kim Weible
  * @version cs6241 Fall
  */
 public abstract class GradeStrategyDecorator implements GradeStrategy {
 
-	public void doAdditional() {};
-	
+	private GradeStrategy strategy;
+
+	/**
+	 * Constructor for the GradeStrategyDecorator that accepts the strategy
+	 * needed to apply the additional logic
+	 * 
+	 * @param strategy
+	 *            strategy needed to apply additional logic
+	 */
+	public GradeStrategyDecorator(GradeStrategy strategy) {
+		this.strategy = strategy;
+	}
+
+	/**
+	 * Method that accepts the strategy needed and applies the additional logic
+	 * 
+	 * @param strategy
+	 *            strategy needed to apply additional logic
+	 */
+	public void doAdditional(GradeStrategy strategy) {
+		this.strategy = strategy;
+	}
+
 	@Override
 	public double getSubtotal(ArrayList<Grade> grades) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.strategy.getSubtotal(grades);
 	}
 }
