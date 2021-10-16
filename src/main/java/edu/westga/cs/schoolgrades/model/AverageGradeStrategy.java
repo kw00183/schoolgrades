9c41626152f162c59b5e9ea240b343bb23b5c348
Kim Weible <kw00183@my.westga.edu>
2021-10-16 13:30:52 -0500

    AverageGradeStrategy - stage new class that implements GradeStrategy to average all the grades

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/AverageGradeStrategy.java
@@ -0,0 +1,18 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.ArrayList;
+
+/**
+ * Class for the strategy to average all the grades
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class AverageGradeStrategy implements GradeStrategy {
+
+	@Override
+	public double getSubtotal(ArrayList<Grade> grades) {
+		// TODO Auto-generated method stub
+		return 0;
+	}	
+}
