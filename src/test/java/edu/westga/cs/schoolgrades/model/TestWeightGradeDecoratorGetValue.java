cc5980d6c8f6ba8af73eca97f98a9278b0620597
Kim Weible <kw00183@my.westga.edu>
2021-10-17 10:17:27 -0500

    TestWeightGradeDecoratorGetValue - add in Junit test method to test return of grade value using specified weight

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightGradeDecoratorGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightGradeDecoratorGetValue.java
@@ -1,19 +1,36 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestWeightGradeDecoratorGetValue {
+/**
+ * JUnit test class for WeightGradeDecorator model class getValue method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestWeightGradeDecoratorGetValue {
 
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
+	/**
+	 * Test weighting grade with 0.2 weight
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void setWeightGradeWeight2() {
+		double gradeToWeight = 99.0;
 
+		WeightedGrade decoratedGrade = new WeightedGrade();
+		WeightGradeDecorator decorator = new WeightGradeDecorator(decoratedGrade);
+		decoratedGrade.setValue(gradeToWeight);
+		
+		decorator.setDecoratedGrade(decoratedGrade);
+		decorator.setWeight(0.2);
+
+		assertEquals(19.8, decorator.getValue());
+	}
 }
