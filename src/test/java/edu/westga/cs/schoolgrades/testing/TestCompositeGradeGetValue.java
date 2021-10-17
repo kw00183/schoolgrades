048cb910e493b8ea387737f09254f46c86215074
Kim Weible <kw00183@my.westga.edu>
2021-10-17 00:20:30 -0500

    TestCompositeGradeGetValue - add in JUnit test methods for getValue() method of CompositeGrade

--- src/test/java/edu/westga/cs/schoolgrades/testing/TestCompositeGradeGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/testing/TestCompositeGradeGetValue.java
@@ -1,19 +1,79 @@
 package edu.westga.cs.schoolgrades.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestCompositeGradeGetValue {
+import edu.westga.cs.schoolgrades.model.CompositeGrade;
+
+/**
+ * JUnit test class for CompositeGrade model class getValue method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestCompositeGradeGetValue {
+
+	private CompositeGrade composite;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.composite = new CompositeGrade();
 	}
 
+	/**
+	 * Test to check that getValue method returns correct value for sum
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void getValueCompositeGradeSum() {
+		this.composite.setArray(10.0);
+		this.composite.setArray(20.0);
+		this.composite.setArray(20.0);
+
+		this.composite.setGradeStrategy("sum");
+		assertEquals(50.0, this.composite.getValue());
 	}
 
+	/**
+	 * Test to check that getValue method returns correct value for average
+	 */
+	@Test
+	public void getValueCompositeGradeAverage() {
+		this.composite.setArray(100.0);
+		this.composite.setArray(80.0);
+		this.composite.setArray(75.0);
+
+		this.composite.setGradeStrategy("average");
+		assertEquals(85.0, this.composite.getValue());
+	}
+
+	/**
+	 * Test to check that getValue method returns correct value for drop|average
+	 */
+	@Test
+	public void getValueCompositeGradeDropLowestAverage() {
+		this.composite.setArray(100.0);
+		this.composite.setArray(80.0);
+		this.composite.setArray(75.0);
+
+		this.composite.setGradeStrategy("drop|average");
+		assertEquals(90.0, this.composite.getValue());
+	}
+
+	/**
+	 * Test to check that getValue method throws error for invalid strategy
+	 */
+	@Test
+	public void getValueCompositeGradeInvalidStrategy() {
+		this.composite.setArray(100.0);
+		this.composite.setArray(80.0);
+		this.composite.setArray(75.0);
+
+		try {
+			this.composite.setGradeStrategy("drop|sum");
+		} catch (IllegalArgumentException iae) {
+			assertEquals("invalid strategy", iae.getMessage());
+		}
+	}
 }
