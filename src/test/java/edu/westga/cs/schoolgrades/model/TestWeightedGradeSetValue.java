181f6af7a663bc275fa3078aa3dbcb058aa091c8
Kim Weible <kw00183@my.westga.edu>
2021-10-17 01:56:47 -0500

    TestWeightedGradeSetValue - add in JUnit test methods to setValue() method of WeightedGrade class

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeSetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeSetValue.java
@@ -1,19 +1,59 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestWeightedGradeSetValue {
+/**
+ * JUnit test class for WeightedGrade model class setValue method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestWeightedGradeSetValue {
+
+	private WeightedGrade weighted;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.weighted = new WeightedGrade();
 	}
 
+	/**
+	 * Test to check that setValue method with negative grade throws error
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void setValueWithInvalidNegativeGradeThrowsError() {
+		try {
+			this.weighted.setValue(-90.0);
+		} catch (IllegalArgumentException iae) {
+			assertEquals(
+					"grade must be positive or less than or equal to 100.0",
+					iae.getMessage());
+		}
 	}
 
+	/**
+	 * Test to check that setValue method with high grade throws error
+	 */
+	@Test
+	public void setValueWithInvalidHighGradeThrowsError() {
+		try {
+			this.weighted.setValue(105.0);
+		} catch (IllegalArgumentException iae) {
+			assertEquals(
+					"grade must be positive or less than or equal to 100.0",
+					iae.getMessage());
+		}
+	}
+
+	/**
+	 * Test to check that setValue method adds with valid grade
+	 */
+	@Test
+	public void setArrayWithValidGrade() {
+		this.weighted.setValue(80.0);
+		assertEquals(80.0, this.weighted.getValue());
+	}
 }
