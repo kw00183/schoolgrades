5668a37f80629416d7de499971813396902b6e10
Kim Weible <kw00183@my.westga.edu>
2021-10-17 02:06:03 -0500

    TestWeightedGradeGetValue - stage new JUnit test class

---
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
@@ -0,0 +1,19 @@
+package edu.westga.cs.schoolgrades.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestWeightedGradeGetValue {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
