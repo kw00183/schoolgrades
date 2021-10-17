be88ec69a7351d2df57e9df902b6a8f4251f692f
Kim Weible <kw00183@my.westga.edu>
2021-10-17 12:10:06 -0500

    TestGradeDecoratorGetValue - add in JUnit test method for testing getValue method

--- src/test/java/edu/westga/cs/schoolgrades/model/TestGradeDecoratorGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestGradeDecoratorGetValue.java
@@ -1,19 +1,41 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestGradeDecoratorGetValue {
+/**
+ * JUnit test class for GradeDecorator model class getValue method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestGradeDecoratorGetValue {
+
+	private DummyGradeDecorator dummyDecorator;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.dummyDecorator = new DummyGradeDecorator(
+				this.dummyDecorator);
 	}
 
+	/**
+	 * Test use dummy method to test abstract GradeDecorator getValue
+	 * method
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void getValueGradeDecoratorWith1Grade() {
+		double gradeToWeight = 99.0;
+		
+		WeightedGrade decoratedGrade = new WeightedGrade();
+		this.dummyDecorator = new DummyGradeDecorator(
+				decoratedGrade);
+		
+		decoratedGrade.setValue(gradeToWeight);
+		this.dummyDecorator.setDecoratedGrade(decoratedGrade);
 
+		assertEquals(99.0, this.dummyDecorator.getValue());
+	}
 }
