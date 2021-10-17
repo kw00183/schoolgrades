dc1c07500d2b82cd02af23edfd5591ba2c3268fb
Kim Weible <kw00183@my.westga.edu>
2021-10-17 02:25:44 -0500

    WeightGradeDecorator - add in javadoc comments

--- src/main/java/edu/westga/cs/schoolgrades/model/WeightGradeDecorator.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/WeightGradeDecorator.java
@@ -1,11 +1,26 @@
 package edu.westga.cs.schoolgrades.model;
 
+/**
+ * Class for the grade decorator to add additional logic the grades
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
 public class WeightGradeDecorator extends GradeDecorator {
 
 	private Grade oldGrade;
 	private double newGrade;
 	private double gradeWeight;
 
+	/**
+	 * Constructor for the GradeDecorator that accepts the grade to decorate and
+	 * the weight to use
+	 * 
+	 * @param decoratedGrade
+	 *            grade needed to apply additional logic
+	 * @param gradeWeight
+	 *            weight needed to apply to grade
+	 */
 	public WeightGradeDecorator(Grade decoratedGrade, double gradeWeight) {
 		super(decoratedGrade);
 		this.gradeWeight = gradeWeight;
@@ -22,10 +37,11 @@ public class WeightGradeDecorator extends GradeDecorator {
 	 * 
 	 * @param gradeWeight
 	 *            weight needed to multply with the grade
+	 * @return newGrade weighted grade
 	 */
 	public double weightGrade(double gradeWeight) {
 		this.oldGrade = super.getDecoratedGrade();
 		this.newGrade = this.oldGrade.getValue() * gradeWeight;
-		return newGrade;
+		return this.newGrade;
 	}
 }
