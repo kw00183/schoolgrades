fbea02b37a8509b7a3271621e0eac64f95156f6a
Kim Weible <kw00183@my.westga.edu>
2021-10-16 20:15:35 -0500

    TestAverageGradeStrategyGetSubtotal - add in JUnit tests to check for throw errors of null and empty array values as well as correct averages returned for 1 or more grade values

--- src/test/java/edu/westga/cs/schoolgrades/testing/TestAverageGradeStrategyGetSubtotal.java
+++ src/test/java/edu/westga/cs/schoolgrades/testing/TestAverageGradeStrategyGetSubtotal.java
@@ -1,19 +1,91 @@
 package edu.westga.cs.schoolgrades.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestAverageGradeStrategyGetSubtotal {
+import edu.westga.cs.schoolgrades.model.AverageGradeStrategy;
+
+/**
+ * JUnit test class for SumGradeStrategy model class getSubtotal method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestAverageGradeStrategyGetSubtotal {
+
+	private AverageGradeStrategy strategy;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.strategy = new AverageGradeStrategy();
 	}
 
+	/**
+	 * Test to check that creating AverageGradeStrategy with null parameter
+	 * throws error
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void getSubtotalAverageGradeStrategyWithNullParameterThrowsError() {
+		try {
+			this.strategy.getSubtotal(null);
+		} catch (NullPointerException npe) {
+			assertEquals("grades cannot be null or empty", npe.getMessage());
+		}
 	}
 
+	/**
+	 * Test to check that creating AverageGradeStrategy with empty array throws
+	 * error
+	 */
+	@Test
+	public void getSubtotalAverageGradeStrategyWithEmptyArrayThrowsError() {
+		ArrayList<Double> testArray = new ArrayList<Double>();
+
+		try {
+			this.strategy.getSubtotal(testArray);
+		} catch (NullPointerException npe) {
+			assertEquals("grades cannot be null or empty", npe.getMessage());
+		}
+	}
+	
+	/**
+	 * Test to check that creating AverageGradeStrategy with 1 grade returns average equal to grade
+	 */
+	@Test
+	public void getSubtotalAverageGradeStrategyWithOneGrade() {
+		ArrayList<Double> testArray = new ArrayList<Double>();
+		testArray.add(10.0);
+
+		assertEquals(10.0, this.strategy.getSubtotal(testArray));
+	}
+	
+	/**
+	 * Test to check that creating AverageGradeStrategy with 2 grades returns correct average
+	 */
+	@Test
+	public void getSubtotalAverageGradeStrategyWithTwoGrades() {
+		ArrayList<Double> testArray = new ArrayList<Double>();
+		testArray.add(100.0);
+		testArray.add(70.0);
+
+		assertEquals(85.0, this.strategy.getSubtotal(testArray));
+	}
+	
+	/**
+	 * Test to check that creating AverageGradeStrategy with 3 grades (1 is 0.0) returns correct average
+	 */
+	@Test
+	public void getSubtotalAverageGradeStrategyWithThreeGradesOne0() {
+		ArrayList<Double> testArray = new ArrayList<Double>();
+		testArray.add(100.0);
+		testArray.add(70.0);
+		testArray.add(0.0);
+
+		Double roundAverageGrade = Math.round(this.strategy.getSubtotal(testArray) * 100.0) / 100.0;
+		assertEquals(56.67, roundAverageGrade);
+	}
 }
