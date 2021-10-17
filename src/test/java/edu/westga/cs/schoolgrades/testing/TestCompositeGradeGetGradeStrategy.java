15614bb33772212d8009d1bde2b2bddc7f125456
Kim Weible <kw00183@my.westga.edu>
2021-10-17 00:01:22 -0500

    TestCompositeGradeGetGradeStrategy - add in JUnit tests for getGradeStrategy() method

--- src/test/java/edu/westga/cs/schoolgrades/testing/TestCompositeGradeGetGradeStrategy.java
+++ src/test/java/edu/westga/cs/schoolgrades/testing/TestCompositeGradeGetGradeStrategy.java
@@ -1,19 +1,33 @@
 package edu.westga.cs.schoolgrades.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestCompositeGradeGetGradeStrategy {
+import edu.westga.cs.schoolgrades.model.CompositeGrade;
+
+/**
+ * JUnit test class for CompositeGrade model class getGradeStrategy method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestCompositeGradeGetGradeStrategy {
+
+	private CompositeGrade composite;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.composite = new CompositeGrade();
 	}
 
+	/**
+	 * Test to check that getGradeStrategy method returns "sum"
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void getGradeStrategyWithSum() {
+		this.composite.setGradeStrategy("sum");
+		assertEquals("sum", this.composite.getGradeStrategy());
 	}
-
 }
