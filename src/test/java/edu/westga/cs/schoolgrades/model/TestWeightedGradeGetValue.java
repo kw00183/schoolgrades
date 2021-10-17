64fa5228209c6d293cd0c57e84795df2b3f70af8
Kim Weible <kw00183@my.westga.edu>
2021-10-17 02:09:14 -0500

    TestWeightedGradeGetValue - add in JUnit test method to test getValue

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
@@ -1,19 +1,31 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestWeightedGradeGetValue {
+/**
+ * JUnit test class for WeightedGrade model class getValue method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestWeightedGradeGetValue {
+
+	private WeightedGrade weighted;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.weighted = new WeightedGrade();
 	}
 
+	/**
+	 * Test to check that getValue method returns valid grade
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void getArrayWithValidGrade() {
+		this.weighted.setValue(80.0);
+		assertEquals(80.0, this.weighted.getValue());
 	}
-
 }
