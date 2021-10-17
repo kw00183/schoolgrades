5be0c168c7050b9c10bfb83fed757bd124d9f6d0
Kim Weible <kw00183@my.westga.edu>
2021-10-16 22:18:47 -0500

    DropLowestGradeDecorator - add in logic to pass in a strategy, drop the lowest grade and then find the grade subtotal using the strategy

--- src/main/java/edu/westga/cs/schoolgrades/model/DropLowestGradeDecorator.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/DropLowestGradeDecorator.java
@@ -1,26 +1,53 @@
 package edu.westga.cs.schoolgrades.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
+/**
+ * Class for the strategy decorator to drop lowest grade and apply strategy
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
 public class DropLowestGradeDecorator extends GradeStrategyDecorator {
 
+	/**
+	 * Constructor accepts strategy to decorate with additional logic
+	 * 
+	 * @param decoratedStrategy
+	 *            the strategy to decorate with additional logic
+	 */
 	public DropLowestGradeDecorator(GradeStrategy decoratedStrategy) {
 		super(decoratedStrategy);
 	}
 
 	@Override
-	public double getSubtotal(ArrayList<Grade> grades) {
-		ArrayList<Grade> newGrades = this.dropLowestGrade(grades);
+	public double getSubtotal(ArrayList<Double> grades) {
+		ArrayList<Double> newGrades = this.dropLowestGrade(grades);
 		return super.getDecoratedStrategy().getSubtotal(newGrades);
 	}
-	
+
 	/**
-	 * Method that accepts the strategy needed and applies the additional logic
+	 * Method that drops the lowest grade from the list of grades
 	 * 
 	 * @param grades
-	 *            strategy needed to apply additional logic
+	 *            grades to evaluate and drop lowest
+	 * @return newGrades	new list of grades to apply strategy
 	 */
-	public ArrayList<Grade> dropLowestGrade(ArrayList<Grade> grades) {
-		return grades;
+	public ArrayList<Double> dropLowestGrade(ArrayList<Double> grades) {
+		ArrayList<Double> newGrades = new ArrayList<Double>();
+		
+		if (grades == null || grades.size() == 0) {
+			throw new NullPointerException("grades cannot be null or empty");
+		} else if (grades.size() == 1) {
+			newGrades = grades;
+		} else {
+			Collections.sort(grades, Collections.reverseOrder());
+			
+			for (int index = 0; index < grades.size() - 1; index++) {
+				newGrades.add(grades.get(index));
+			}	
+		}
+		return newGrades;
 	}
 }
