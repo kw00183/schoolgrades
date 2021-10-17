f2399199e01893a0f90b636e681501b04d32cc13
Kim Weible <kw00183@my.westga.edu>
2021-10-17 11:53:57 -0500

    TestSimpleGradeSetValue - add in JUnit test methods that test invalid and valid grades

--- src/test/java/edu/westga/cs/schoolgrades/model/TestSimpleGradeSetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestSimpleGradeSetValue.java
@@ -1,19 +1,59 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestSimpleGradeSetValue {
+/**
+ * JUnit test class for Simple Grade model class setValue method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestSimpleGradeSetValue {
+
+	private SimpleGrade grade;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.grade = new SimpleGrade();
 	}
 
+	/**
+	 * Test to check that setting a valid grade returns set result
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void setValueWithValidGrade() {
+		this.grade.setValue(99.0);
+		assertEquals(99.0, this.grade.getValue());
 	}
 
+	/**
+	 * Test to check that setting negative grades throws error
+	 */
+	@Test
+	public void setValueWithNegativeGradeThrowError() {
+		try {
+			this.grade.setValue(-99.0);
+		} catch (IllegalArgumentException iae) {
+			assertEquals(
+					"grade must be positive or less than or equal to 100.0",
+					iae.getMessage());
+		}
+	}
+	
+	/**
+	 * Test to check that setting high grades throws error
+	 */
+	@Test
+	public void setValueWithHighGradeThrowError() {
+		try {
+			this.grade.setValue(105.0);
+		} catch (IllegalArgumentException iae) {
+			assertEquals(
+					"grade must be positive or less than or equal to 100.0",
+					iae.getMessage());
+		}
+	}
 }
