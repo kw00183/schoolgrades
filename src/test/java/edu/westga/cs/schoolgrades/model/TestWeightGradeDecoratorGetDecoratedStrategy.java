6da57626cab159a0d7c10b320efe75297d8f7306
Kim Weible <kw00183@my.westga.edu>
2021-10-17 11:33:15 -0500

    TestWeightGradeDecoratorGetDecoratedStrategy - add in JUnit test methods to test the getDecoratedStrategy in the GradeStrategyDecorator class using Dummy

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightGradeDecoratorGetDecoratedStrategy.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightGradeDecoratorGetDecoratedStrategy.java
@@ -1,19 +1,57 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestWeightGradeDecoratorGetDecoratedStrategy {
+/**
+ * JUnit test class for GradeStrategyDecorator model class getDecoratedStrategy method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestWeightGradeDecoratorGetDecoratedStrategy {
+
+	private DummyGradeStrategyDecorator dummyDecorator;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.dummyDecorator = new DummyGradeStrategyDecorator(
+				this.dummyDecorator);
 	}
 
+	/**
+	 * Test use dummy method to test abstract GradeStrategyDecorator getDecoratedStrategy
+	 * method for sum strategy with 1 grade
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void getDecoratedStrategyGradeStrategyDecoratorWith1GradeSumStrategy() {
+		ArrayList<Double> testArray = new ArrayList<Double>();
+		testArray.add(100.0);
 
+		GradeStrategy decoratedStrategy = new SumGradeStrategy();
+		this.dummyDecorator = new DummyGradeStrategyDecorator(
+				decoratedStrategy);
+
+		Assert.assertNotNull(this.dummyDecorator.getDecoratedStrategy());
+	}
+	
+	/**
+	 * Test use dummy method to test abstract GradeStrategyDecorator getDecoratedStrategy
+	 * method for sum strategy with 2 grades
+	 */
+	@Test
+	public void getDecoratedStrategyGradeStrategyDecoratorWith2GradesSumStrategy() {
+		ArrayList<Double> testArray = new ArrayList<Double>();
+		testArray.add(100.0);
+		testArray.add(180.0);
+
+		GradeStrategy decoratedStrategy = new SumGradeStrategy();
+		this.dummyDecorator = new DummyGradeStrategyDecorator(
+				decoratedStrategy);
+
+		Assert.assertNotNull(this.dummyDecorator.getDecoratedStrategy());
+	}
 }
