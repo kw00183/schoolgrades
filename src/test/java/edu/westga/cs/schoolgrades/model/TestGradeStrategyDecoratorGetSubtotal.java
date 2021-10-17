ae3afcf3e3d48f5f7e4f528f5829ea074fcc2fd8
Kim Weible <kw00183@my.westga.edu>
2021-10-17 11:22:52 -0500

    TestGradeStrategyDecoratorGetSubtotal - add in JUnit test methods to test getSubtotal method

--- src/test/java/edu/westga/cs/schoolgrades/model/TestGradeStrategyDecoratorGetSubtotal.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestGradeStrategyDecoratorGetSubtotal.java
@@ -1,19 +1,58 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestGradeStrategyDecoratorGetSubtotal {
+/**
+ * JUnit test class for GradeStrategyDecorator model class getSubtotal method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestGradeStrategyDecoratorGetSubtotal {
+
+	private DummyGradeStrategyDecorator dummyDecorator;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.dummyDecorator = new DummyGradeStrategyDecorator(
+				this.dummyDecorator);
 	}
 
+	/**
+	 * Test use dummy method to test abstract GradeStrategyDecorator getSubtotal
+	 * method for sum strategy with 1 grade
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void getSubtotalGradeStrategyDecoratorWith1GradeSumStrategy() {
+		ArrayList<Double> testArray = new ArrayList<Double>();
+		testArray.add(100.0);
 
+		GradeStrategy decoratedStrategy = new SumGradeStrategy();
+		this.dummyDecorator = new DummyGradeStrategyDecorator(
+				decoratedStrategy);
+
+		assertEquals(100.0, this.dummyDecorator.getSubtotal(testArray));
+	}
+	
+	/**
+	 * Test use dummy method to test abstract GradeStrategyDecorator getSubtotal
+	 * method for sum strategy with 2 grades
+	 */
+	@Test
+	public void getSubtotalGradeStrategyDecoratorWith2GradesSumStrategy() {
+		ArrayList<Double> testArray = new ArrayList<Double>();
+		testArray.add(100.0);
+		testArray.add(80.0);
+
+		GradeStrategy decoratedStrategy = new SumGradeStrategy();
+		this.dummyDecorator = new DummyGradeStrategyDecorator(
+				decoratedStrategy);
+
+		assertEquals(180.0, this.dummyDecorator.getSubtotal(testArray));
+	}
 }
