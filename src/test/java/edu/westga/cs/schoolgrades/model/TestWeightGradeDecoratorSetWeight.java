c65ad2b8c55bbefb81f099d47163836b1ecf9a63
Kim Weible <kw00183@my.westga.edu>
2021-10-17 09:58:31 -0500

    TestWeightGradeDecoratorSetWeight - add in JUnit test methods for testing grades with different weight values

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightGradeDecoratorSetWeight.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightGradeDecoratorSetWeight.java
@@ -1,19 +1,70 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestWeightGradeDecoratorSetWeight {
+/**
+ * JUnit test class for WeightGradeDecorator model class setWeight method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestWeightGradeDecoratorSetWeight {
 
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
+	/**
+	 * Test weighting grade with 0.2 weight
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void getValueWeightGradeWeight2() {
+		double gradeToWeight = 99.0;
 
+		WeightedGrade decoratedGrade = new WeightedGrade();
+		WeightGradeDecorator decorator = new WeightGradeDecorator(decoratedGrade);
+		decoratedGrade.setValue(gradeToWeight);
+		
+		decorator.setDecoratedGrade(decoratedGrade);
+		decorator.setWeight(0.2);
+
+		assertEquals(19.8, decorator.getValue());
+	}
+	
+	/**
+	 * Test weighting grade with 0.3 weight
+	 */
+	@Test
+	public void getValueWeightGradeWeight3() {
+		double gradeToWeight = 99.0;
+
+		WeightedGrade decoratedGrade = new WeightedGrade();
+		WeightGradeDecorator decorator = new WeightGradeDecorator(decoratedGrade);
+		decoratedGrade.setValue(gradeToWeight);
+		
+		decorator.setDecoratedGrade(decoratedGrade);
+		decorator.setWeight(0.3);
+
+		assertEquals(29.7, decorator.getValue());
+	}
+	
+	/**
+	 * Test weighting grade with 0.5 weight
+	 */
+	@Test
+	public void getValueWeightGradeWeight5() {
+		double gradeToWeight = 99.0;
+
+		WeightedGrade decoratedGrade = new WeightedGrade();
+		WeightGradeDecorator decorator = new WeightGradeDecorator(decoratedGrade);
+		decoratedGrade.setValue(gradeToWeight);
+		
+		decorator.setDecoratedGrade(decoratedGrade);
+		decorator.setWeight(0.5);
+
+		assertEquals(49.5, decorator.getValue());
+	}
 }
