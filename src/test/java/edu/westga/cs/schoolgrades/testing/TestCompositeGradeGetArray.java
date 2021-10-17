59b530d591455c1247508e11cd789a6344240937
Kim Weible <kw00183@my.westga.edu>
2021-10-16 23:16:30 -0500

    TestCompositeGradeGetArray - add in JUnit test methods for getArray method in CompositeGrade class

--- src/test/java/edu/westga/cs/schoolgrades/testing/TestCompositeGradeGetArray.java
+++ src/test/java/edu/westga/cs/schoolgrades/testing/TestCompositeGradeGetArray.java
@@ -1,19 +1,54 @@
 package edu.westga.cs.schoolgrades.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestCompositeGradeGetArray {
+import edu.westga.cs.schoolgrades.model.CompositeGrade;
+
+/**
+ * JUnit test class for CompositeGrade model class getArray method
+ * 
+ * @author Kim Weible
+ * @version cs6241 Fall
+ */
+public class TestCompositeGradeGetArray {
+
+	private CompositeGrade composite;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.composite = new CompositeGrade();
 	}
 
+	/**
+	 * Test to check that setArray method adds with 1 valid grade
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void setArrayWith1ValidGrade() {
+		this.composite.setArray(80.0);
+		assertEquals("[80.0]", this.composite.getArray().toString());
 	}
-
+	
+	/**
+	 * Test to check that setArray method adds with 2 valid grades
+	 */
+	@Test
+	public void setArrayWith2ValidGrades() {
+		this.composite.setArray(80.0);
+		this.composite.setArray(0.0);
+		assertEquals("[80.0, 0.0]", this.composite.getArray().toString());
+	}
+	
+	/**
+	 * Test to check that setArray method adds with 3 valid grades
+	 */
+	@Test
+	public void setArrayWith3ValidGrades() {
+		this.composite.setArray(80.0);
+		this.composite.setArray(50.0);
+		this.composite.setArray(0.0);
+		assertEquals("[80.0, 50.0, 0.0]", this.composite.getArray().toString());
+	}
 }
