aa2208b43ac7834bba26d75e7272e84e3229772b
Kim Weible <kw00183@my.westga.edu>
2021-11-01 20:57:02 -0500

    Main - add in private variable, start method and main method

--- src/main/java/edu/westga/cs/schoolgrades/Main.java
+++ src/main/java/edu/westga/cs/schoolgrades/Main.java
@@ -1,9 +1,45 @@
 package edu.westga.cs.schoolgrades;
 
-public class Main {
+import java.net.URL;
 
-	public static void main(String[] args) {
-		
-	}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+/**
+ * Main class for the SchoolGrades project
+ * 
+ * @author Kim Weible
+ * @version cs6241
+ */
+public class Main extends Application {
+	
+	private static final String GUI_RESOURCE = "edu/westga/cs/schoolgrades/views/MainGui.fxml";
+	
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL resource = classLoader.getResource(Main.GUI_RESOURCE);
+        FXMLLoader loader = new FXMLLoader(resource);
+        
+        primaryStage.setTitle("Grades Worksheet");
+		primaryStage.setResizable(false);
+        
+        Parent root = (Parent) loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Grades Worksheet");
+        primaryStage.show();
+    }
+
+	/**
+     * Start point for the application
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
