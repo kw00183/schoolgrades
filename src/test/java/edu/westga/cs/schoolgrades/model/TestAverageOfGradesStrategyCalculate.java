2013c00197c1b97989bd55bcd67eb48bbb56b770
Kim Weible <kw00183@my.westga.edu>
2021-11-06 16:10:46 -0500

    TestAverageOfGradesStrategyCalculate - fix checkstyle issues

--- src/test/java/edu/westga/cs/schoolgrades/model/TestAverageOfGradesStrategyCalculate.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestAverageOfGradesStrategyCalculate.java
@@ -1,63 +1,81 @@
 package edu.westga.cs.schoolgrades.model;
 
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-
-
-
+/**
+ * Tests for AverageOfGradesStrategy class calculation
+ * 
+ * @author lewisb
+ * @version cs6241
+ */
 public class TestAverageOfGradesStrategyCalculate {
 
 	private static final double DELTA = 0.001;
 	private Grade grade0;
 	private Grade grade1;
 	private Grade grade2;
-	
+
 	private List<Grade> grades;
-	
+
 	private AverageOfGradesStrategy strategy;
-	
+
+	/**
+	 * setup method run before each test
+	 */
 	@BeforeEach
 	public void setup() {
-		grade0 = new SimpleGrade(10);
-		grade1 = new SimpleGrade(20);
-		grade2 = new SimpleGrade(30);
-		
-		grades = new ArrayList<Grade>();
-		
-		strategy = new AverageOfGradesStrategy();
-	}
-	
-	@Test
-	public  void shouldNotAllowNullGradesList() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
-			strategy.calculate(null);
-		});
-	}
-	
-	@Test
-	public void shouldGiveZeroIfNoGrades() {
-		assertEquals(0, strategy.calculate(grades), DELTA);
-	}
-	
-	@Test
-	public void shouldCalculateAverageOfOneGrades() {
-		grades.add(grade0);
-		assertEquals(grade0.getValue(), strategy.calculate(grades), DELTA);
+		this.grade0 = new SimpleGrade(10);
+		this.grade1 = new SimpleGrade(20);
+		this.grade2 = new SimpleGrade(30);
+
+		this.grades = new ArrayList<Grade>();
+
+		this.strategy = new AverageOfGradesStrategy();
 	}
 
+	/**
+	 * test null grade
+	 */
+	@Test
+	public void shouldNotAllowNullGradesList() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			this.strategy.calculate(null);
+		});
+	}
+
+	/**
+	 * test empty grade
+	 */
+	@Test
+	public void shouldGiveZeroIfNoGrades() {
+		assertEquals(0, this.strategy.calculate(this.grades), DELTA);
+	}
+
+	/**
+	 * test average strategy with one grade
+	 */
+	@Test
+	public void shouldCalculateAverageOfOneGrades() {
+		this.grades.add(this.grade0);
+		assertEquals(this.grade0.getValue(),
+				this.strategy.calculate(this.grades), DELTA);
+	}
+
+	/**
+	 * test average strategy with many grades
+	 */
 	@Test
 	public void shouldCalculateSumOManyGrades() {
-		grades.add(grade0);
-		grades.add(grade1);
-		grades.add(grade2);
-		assertEquals(20, strategy.calculate(grades), DELTA);
+		this.grades.add(this.grade0);
+		this.grades.add(this.grade1);
+		this.grades.add(this.grade2);
+		assertEquals(20, this.strategy.calculate(this.grades), DELTA);
 	}
 
 }
