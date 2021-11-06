90ff38d60ea172730eb8a66b7d00cd41f5c73c03
Kim Weible <kw00183@my.westga.edu>
2021-11-06 16:20:37 -0500

    TestCompositeGradeAddAll - fix checkstyle issues

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeAddAll.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeAddAll.java
@@ -1,13 +1,20 @@
 package edu.westga.cs.schoolgrades.model;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests for CompositeGrade class addAll method
+ * 
+ * @author lewisb
+ * @version cs6241
+ */
 public class TestCompositeGradeAddAll {
 
 	private CompositeGrade composite;
@@ -15,49 +22,63 @@ public class TestCompositeGradeAddAll {
 	private Grade grade1;
 	private Grade grade2;
 	private List<Grade> list;
-	
+
+	/**
+	 * setup method run before each test
+	 */
 	@BeforeEach
 	public void setup() {
-		composite = new CompositeGrade(new SumOfGradesStrategy());
-		grade0 = new SimpleGrade(10);
-		grade1 = new SimpleGrade(20);
-		grade2 = new SimpleGrade(30);
-		list = new ArrayList<Grade>();
+		this.composite = new CompositeGrade(new SumOfGradesStrategy());
+		this.grade0 = new SimpleGrade(10);
+		this.grade1 = new SimpleGrade(20);
+		this.grade2 = new SimpleGrade(30);
+		this.list = new ArrayList<Grade>();
 	}
-	
 
+	/**
+	 * test for add of null list of grades
+	 */
 	@Test
 	public void shouldNotAddNullGradesList() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
-			composite.addAll(null);
+		assertThrows(IllegalArgumentException.class, () -> {
+			this.composite.addAll(null);
 		});
 	}
-	
+
+	/**
+	 * test for add of empty list of grades
+	 */
 	@Test
 	public void shouldAddEmptyList() {
-		composite.addAll(new ArrayList<Grade>());
-		assertTrue(composite.getGrades().isEmpty());
+		this.composite.addAll(new ArrayList<Grade>());
+		assertTrue(this.composite.getGrades().isEmpty());
 	}
-	
+
+	/**
+	 * test for add of one grade in list
+	 */
 	@Test
 	public void shouldAddOneElementList() {
-		list.add(grade0);
-		composite.addAll(list);
-		List<Grade> actual = composite.getGrades();
+		this.list.add(this.grade0);
+		this.composite.addAll(this.list);
+		List<Grade> actual = this.composite.getGrades();
 		assertEquals(1, actual.size());
-		assertEquals(grade0, actual.get(0));
+		assertEquals(this.grade0, actual.get(0));
 	}
-	
+
+	/**
+	 * test for add of many grades in list
+	 */
 	@Test
 	public void shouldAddManyElementsList() {
-		list.add(grade0);
-		list.add(grade1);
-		list.add(grade2);
-		composite.addAll(list);
-		List<Grade> actual = composite.getGrades();
+		this.list.add(this.grade0);
+		this.list.add(this.grade1);
+		this.list.add(this.grade2);
+		this.composite.addAll(this.list);
+		List<Grade> actual = this.composite.getGrades();
 		assertEquals(3, actual.size());
-		assertEquals(grade0, actual.get(0));
-		assertEquals(grade1, actual.get(1));
-		assertEquals(grade2, actual.get(2));
+		assertEquals(this.grade0, actual.get(0));
+		assertEquals(this.grade1, actual.get(1));
+		assertEquals(this.grade2, actual.get(2));
 	}
 }
