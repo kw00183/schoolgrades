8291e49e1e9555bbf14a5301892134db871090d6
Kim Weible <kw00183@my.westga.edu>
2021-11-06 16:24:21 -0500

    TestCompositeGradeConstructor - fix checckstyle issues

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeConstructor.java
@@ -1,19 +1,30 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests for CompositeGrade class constructor
+ * 
+ * @author lewisb
+ * @version cs6241
+ */
 public class TestCompositeGradeConstructor {
 
-	
-	
+	/**
+	 * test of null strategy
+	 */
 	@Test
 	public void shouldNotAllowNullStrategy() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
+		assertThrows(IllegalArgumentException.class, () -> {
 			new CompositeGrade(null);
 		});
 	}
-	
+
+	/**
+	 * test empty composite grade
+	 */
 	@Test
 	public void shouldHaveNoGradesWhenCreated() {
 		CompositeGrade grade = new CompositeGrade(new SumOfGradesStrategy());
