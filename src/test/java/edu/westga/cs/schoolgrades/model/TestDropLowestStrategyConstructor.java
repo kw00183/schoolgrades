c5c4923d18a8f3eb374aa4f02ece0c2377dbf720
Kim Weible <kw00183@my.westga.edu>
2021-11-06 16:33:13 -0500

    TestDropLowestStrategyConstructor - fix checkstyle issues

--- src/test/java/edu/westga/cs/schoolgrades/model/TestDropLowestStrategyConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestDropLowestStrategyConstructor.java
@@ -1,18 +1,30 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests for DropLowestStrategy class constructor
+ * 
+ * @author lewisb
+ * @version cs6241
+ */
 public class TestDropLowestStrategyConstructor {
 
+	/**
+	 * setup method run before each test
+	 */
 	@BeforeEach
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * test with null strategy
+	 */
 	@Test
 	public void shouldNotAllowNullChildStrategy() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
+		assertThrows(IllegalArgumentException.class, () -> {
 			new DropLowestStrategy(null);
 		});
 	}
