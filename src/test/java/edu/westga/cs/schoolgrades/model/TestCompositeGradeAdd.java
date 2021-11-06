a633acbb84993c664434c97324584f8ea18c37f1
Kim Weible <kw00183@my.westga.edu>
2021-11-06 16:14:25 -0500

    TestCompositeGradeAdd - fix checkstyle issues

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeAdd.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeAdd.java
@@ -1,52 +1,69 @@
 package edu.westga.cs.schoolgrades.model;
 
-
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests for CompositeGrade class add method
+ * 
+ * @author lewisb
+ * @version cs6241
+ */
 public class TestCompositeGradeAdd {
 
 	private CompositeGrade composite;
 	private Grade grade0;
 	private Grade grade1;
 	private Grade grade2;
-	
+
+	/**
+	 * setup method run before each test
+	 */
 	@BeforeEach
 	public void setup() {
-		composite = new CompositeGrade(new SumOfGradesStrategy());
-		grade0 = new SimpleGrade(10);
-		grade1 = new SimpleGrade(20);
-		grade2 = new SimpleGrade(30);
-	}
-	
-	@Test
-	public void canAddOneGrade() {
-		composite.add(grade0);
-		List<Grade> grades = composite.getGrades();
-		assertEquals(1, grades.size());
-		assertEquals(grade0, grades.get(0));
+		this.composite = new CompositeGrade(new SumOfGradesStrategy());
+		this.grade0 = new SimpleGrade(10);
+		this.grade1 = new SimpleGrade(20);
+		this.grade2 = new SimpleGrade(30);
 	}
 
+	/**
+	 * test adding one grade
+	 */
+	@Test
+	public void canAddOneGrade() {
+		this.composite.add(this.grade0);
+		List<Grade> grades = this.composite.getGrades();
+		assertEquals(1, grades.size());
+		assertEquals(this.grade0, grades.get(0));
+	}
+
+	/**
+	 * test adding many grades
+	 */
 	@Test
 	public void canAddManyGrades() {
-		composite.add(grade0);
-		composite.add(grade1);
-		composite.add(grade2);
-		List<Grade> grades = composite.getGrades();
+		this.composite.add(this.grade0);
+		this.composite.add(this.grade1);
+		this.composite.add(this.grade2);
+		List<Grade> grades = this.composite.getGrades();
 		assertEquals(3, grades.size());
-		assertEquals(grade0, grades.get(0));
-		assertEquals(grade1, grades.get(1));
-		assertEquals(grade2, grades.get(2));
+		assertEquals(this.grade0, grades.get(0));
+		assertEquals(this.grade1, grades.get(1));
+		assertEquals(this.grade2, grades.get(2));
 	}
-	
-	
+
+	/**
+	 * test add of null grade
+	 */
 	@Test
 	public void shouldNotAddNullGrade() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
-			composite.add(null);
+		assertThrows(IllegalArgumentException.class, () -> {
+			this.composite.add(null);
 		});
 	}
 }
