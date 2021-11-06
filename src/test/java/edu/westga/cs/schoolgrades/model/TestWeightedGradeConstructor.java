f41c2d355676bf657191f01f9b723c749fcaaba1
Kim Weible <kw00183@my.westga.edu>
2021-11-06 16:41:53 -0500

    TestWeightedGradeConstructor - fix checkstyle issue

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
@@ -1,28 +1,42 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests for WeightedGrade class constructor
+ * 
+ * @author lewisb
+ * @version cs6241
+ */
 public class TestWeightedGradeConstructor {
 
+	/**
+	 * test weighting null grade
+	 */
 	@Test
 	public void shouldNotAllowNullGrade() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
+		assertThrows(IllegalArgumentException.class, () -> {
 			new WeightedGrade(null, 10.0);
 		});
 	}
-	
+
+	/**
+	 * test using negative weight value
+	 */
 	@Test
 	public void shouldNotAllowNegativeWeight() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
+		assertThrows(IllegalArgumentException.class, () -> {
 			new WeightedGrade(new SimpleGrade(50), -10.0);
 		});
 	}
-	
+
+	/**
+	 * test using weight greater than 1
+	 */
 	@Test
 	public void shouldNotAllowWeightGreaterThanOne() {
-		assertThrows(IllegalArgumentException.class, () ->{ 
+		assertThrows(IllegalArgumentException.class, () -> {
 			new WeightedGrade(new SimpleGrade(50), 10.0);
 		});
 	}
